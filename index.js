6da881fe975194f52e97e89750d91d812988f13f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-17 01:37:38 +0600

    75-4 (Recap) Issue Simple JWT token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+var jwt = require("jsonwebtoken");
 const { query } = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -124,6 +125,20 @@ async function run() {
       }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      console.log(user);
+      res.status(403).send({ accessToken: "" });
     });
 
     app.post("/users", async (req, res) => {
